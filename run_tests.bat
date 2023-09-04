c0393210891e805702f5c5b89651d0f019f28604
Bhavyakp321 <bhavya.kp@terralogic.com>
2023-09-04 16:24:24 +0530

    Initial commit

---
+++ run_tests.bat
@@ -0,0 +1,5 @@
+"C:\Users\Bhavya kP-Int-222\PycharmProjects\Automation_2\venv\Scripts\python.exe" -m robot.run --test "Automation_2.Framework.TestCycles.Module 1.testscenario.register1.Registration Page" . 
+"C:\Users\Bhavya kP-Int-222\PycharmProjects\Automation_2\venv\Scripts\python.exe" -m robot.run --test "Automation_2.Framework.TestCycles.Module 1.testscenario.register2.Registration Page2" . 
+"C:\Users\Bhavya kP-Int-222\PycharmProjects\Automation_2\venv\Scripts\python.exe" -m robot.run --test "Automation_2.Framework.TestCycles.Module 2.testscenario.login1.Login Page1" . 
+"C:\Users\Bhavya kP-Int-222\PycharmProjects\Automation_2\venv\Scripts\python.exe" -m robot.run --test "Automation_2.Framework.TestCycles.Module 2.testscenario.login2.Login Page2" .
+
